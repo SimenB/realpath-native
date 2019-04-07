c8dceac42174298d1134db15d030be866e4ff029
rahul <rahul@rvaidya.com>
2019-04-07 16:40:09 -0700

    Messy solution to Windows RAMdisk issue - async calls need Promise chaining

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-arrow-callback */
+
 'use strict';
 
 const fs = require('fs');
@@ -7,7 +9,30 @@ const promisiedFsRealpath = promisify(fs.realpath);
 
 function realpath(filepath) {
   if (typeof fs.realpath.native === 'function') {
-    return promisify(fs.realpath.native)(filepath);
+    return promisify(fs.realpath.native)(filepath).catch(function(err) {
+      // -4068: EISDIR: illegal operation on a directory, realpath
+      if (err.errno !== -4068) {
+        /* Probably RAM-disk on windows.
+         Go straight to the default js
+         implementation.  Otherwise the
+         fsBinding.realpath call may cause
+         the node runtime to abort.
+       */
+        const fsBinding = process.binding('fs');
+
+        if (fsBinding.realpath) {
+          return new Promise((resolve, reject) => {
+            try {
+              resolve(fsBinding.realpath(filepath, 'utf8'));
+            } catch (e) {
+              reject(e);
+            }
+          });
+        }
+      }
+
+      return promisiedFsRealpath(filepath);
+    });
   }
   const fsBinding = process.binding('fs');
 
@@ -25,17 +50,33 @@ function realpath(filepath) {
 }
 
 function realpathSync(filepath) {
-  if (typeof fs.realpathSync.native === 'function') {
-    return fs.realpathSync.native(filepath);
+  let fallbackToDefault = false;
+  try {
+    if (typeof fs.realpathSync.native === 'function') {
+      return fs.realpathSync.native(filepath);
+    }
+  } catch (err) {
+    // -4068: EISDIR: illegal operation on a directory, realpath
+    if (err.errno === -4068) {
+      /* Probably RAM-disk on windows.
+         Go straight to the default js
+         implementation.  Otherwise the
+         fsBinding.realpath call may cause
+         the node runtime to abort.
+       */
+    }
+    fallbackToDefault = true;
   }
 
-  const fsBinding = process.binding('fs');
+  if (!fallbackToDefault) {
+    const fsBinding = process.binding('fs');
 
-  if (fsBinding.realpath) {
-    try {
-      return fsBinding.realpath(filepath, 'utf8');
-    } catch (err) {
-      /* Probably RAM-disk on windows. */
+    if (fsBinding.realpath) {
+      try {
+        return fsBinding.realpath(filepath, 'utf8');
+      } catch (err) {
+        /* Probably RAM-disk on windows. */
+      }
     }
   }
 
