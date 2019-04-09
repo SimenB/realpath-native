8a9a9cdc622fd0d3cabd5900fc7a3040b81d58ce
rahul <rahul@rvaidya.com>
2019-04-09 15:19:33 -0700

    Clean up implementation and readability by using recursive calls

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* eslint-disable prefer-arrow-callback */
-
 'use strict';
 
 const fs = require('fs');
@@ -7,69 +5,59 @@ const promisify = require('util.promisify');
 
 const promisiedFsRealpath = promisify(fs.realpath);
 
-function realpath(filepath) {
-  if (typeof fs.realpath.native === 'function') {
-    return promisify(fs.realpath.native)(filepath).catch(function(err) {
-      // -4068: EISDIR: illegal operation on a directory, realpath
-      if (err.errno !== -4068) {
-        /* If errno === -4068,
-        Probably RAM-disk on windows.
-         Go straight to the default js
-         implementation.  Otherwise the
-         fsBinding.realpath call may cause
-         the node runtime to abort.
-       */
-        const fsBinding = process.binding('fs');
+function isWinRamDiskError(err) {
+  // -4068: EISDIR: illegal operation on a directory, realpath
+  /* Probably RAM-disk on windows.
+     Go straight to the default js
+     implementation.  Otherwise the
+     fsBinding.realpath call may cause
+     the node runtime to abort.
+  */
+  return err.errno === -4068;
+}
 
-        if (fsBinding.realpath) {
-          return new Promise((resolve, reject) => {
-            try {
-              resolve(fsBinding.realpath(filepath, 'utf8'));
-            } catch (e) {
-              reject(e);
-            }
-          });
+function realpath(filepath, skipToDefault) {
+  if (!skipToDefault) {
+    if (typeof fs.realpath.native === 'function') {
+      return promisify(fs.realpath.native)(filepath).catch(function(err) {
+        // Call this function again and skip straight to the default
+        // js behavior if we've encountered the Windows RAMdisk situation
+        if (isWinRamDiskError(err)) {
+          return realpath(filepath, true);
         }
-      }
+      });
+    }
 
-      return promisiedFsRealpath(filepath);
-    });
-  }
-  const fsBinding = process.binding('fs');
+    const fsBinding = process.binding('fs');
 
-  if (fsBinding.realpath) {
-    return new Promise((resolve, reject) => {
-      try {
-        resolve(fsBinding.realpath(filepath, 'utf8'));
-      } catch (e) {
-        reject(e);
-      }
-    });
+    if (fsBinding.realpath) {
+      return new Promise((resolve, reject) => {
+        try {
+          resolve(fsBinding.realpath(filepath, 'utf8'));
+        } catch (e) {
+          reject(e);
+        }
+      });
+    }
   }
 
   return promisiedFsRealpath(filepath);
 }
 
-function realpathSync(filepath) {
-  let fallbackToDefault = false;
-  try {
-    if (typeof fs.realpathSync.native === 'function') {
-      return fs.realpathSync.native(filepath);
+function realpathSync(filepath, skipToDefault) {
+  if (!skipToDefault) {
+    try {
+      if (typeof fs.realpathSync.native === 'function') {
+        return fs.realpathSync.native(filepath);
+      }
+    } catch (err) {
+      // Call this function again and skip straight to the default
+      // js behavior if we've encountered the Windows RAMdisk situation
+      if (isWinRamDiskError(err)) {
+        return realpathSync(filepath, true);
+      }
     }
-  } catch (err) {
-    // -4068: EISDIR: illegal operation on a directory, realpath
-    if (err.errno === -4068) {
-      /* Probably RAM-disk on windows.
-         Go straight to the default js
-         implementation.  Otherwise the
-         fsBinding.realpath call may cause
-         the node runtime to abort.
-       */
-    }
-    fallbackToDefault = true;
-  }
 
-  if (!fallbackToDefault) {
     const fsBinding = process.binding('fs');
 
     if (fsBinding.realpath) {
